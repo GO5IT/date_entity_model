df6cfe4567e495327c98efb7b41943663bb79cff
Go Sugimoto <go.sugimoto@oeaw.ac.at>
2020-10-12 16:52:12 +0200

    clean up of code & test-files

--- GenerateRDF4DateEntities/utils/test_utils/fetch_q2485userAgent.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-fetch_url_UserAgent.pl -a 'application/rdf+xml'   -u 'http://www.wikidata.org/entity/Q2485' 
-
--- GenerateRDF4DateEntities/utils/test_utils/fetch_q2485wget.sh
+++
@@ -1,2 +0,0 @@
-wget --header='Accept:application/rdf+xml' -O Q2485.xml  http://www.wikidata.org/entity/Q2485 
-
